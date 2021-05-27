a1993aeabb0c24cc7301e2f8303c4c8806f64db5
WParlow <Parlow.whitney@gmail.com>
2021-05-27 10:26:03 -0500

    Consolidating selectors into constants

--- SureQAExam.js
+++ SureQAExam.js
@@ -2,6 +2,12 @@ import { Selector, t } from 'testcafe';
 import { ClientFunction } from 'testcafe';
 const getWindowLocation = ClientFunction(() => window.location);
 
+const span = Selector('span')
+const p = Selector('p')
+const input = Selector('input')
+const div = Selector('div')
+const h3 = Selector('h3')
+
 fixture `Sure QA Take Home Exam`
     .page `https://sure-qa-challenge.vercel.app/`;
 
@@ -10,57 +16,57 @@ test('Happy Path BDD Test', async t => {
         /*Landing page*/
         //Don't enter a zipcode
         await t
-            .click(Selector('span').withText('GET A QUOTE'))
-            .expect(Selector('p').withText('Required').exists).ok()
+            .click(span.withText('GET A QUOTE'))
+            .expect(p.withText('Required').exists).ok()
         await checkURL(false, 'building-material')
 
         //Enter an invalid zip code
         await t
-            .typeText(Selector('input').withAttribute('name', 'postalCode'), '1234')
-            .click(Selector('span').withText('GET A QUOTE'))
-            .expect(Selector('p').withText('Invalid zip code').exists).ok()
+            .typeText(input.withAttribute('name', 'postalCode'), '1234')
+            .click(span.withText('GET A QUOTE'))
+            .expect(p.withText('Invalid zip code').exists).ok()
         await checkURL(false, 'building-material')
         
         //Enter a valid zip code
         await t
-            .selectText(Selector('input').withAttribute('name', 'postalCode'))
-            .typeText(Selector('input').withAttribute('name', 'postalCode'), '12345')
-            .click(Selector('span').withText('GET A QUOTE'))
+            .selectText(input.withAttribute('name', 'postalCode'))
+            .typeText(input.withAttribute('name', 'postalCode'), '12345')
+            .click(span.withText('GET A QUOTE'))
         await checkURL(true, 'building-material')
 
         /*Building Materials:*/
         //Select nothing
         await t
-            .click(Selector('span').withText('NEXT'))
+            .click(span.withText('NEXT'))
         await checkURL(false, 'water-proximity')
         //Select 'Bricks'
         await t
-            .click(Selector('input').withAttribute('value', 'bricks'))
-            .click(Selector('span').withText('NEXT'))
+            .click(input.withAttribute('value', 'bricks'))
+            .click(span.withText('NEXT'))
         await checkURL(true, 'water-proximity')
 
         /*Water Proximity:*/
         //Select nothing
         await t
-            .click(Selector('span').withText('NEXT'))
+            .click(span.withText('NEXT'))
         await checkURL(false, 'quote')
         //Select 'Yes'
         await t
-            .click(Selector('input').withAttribute('name', 'waterProximity').withAttribute('value', 'true'))
-            .click(Selector('span').withText('NEXT'))
+            .click(input.withAttribute('name', 'waterProximity').withAttribute('value', 'true'))
+            .click(span.withText('NEXT'))
         await checkURL(true, 'quote')
 
         /*Quote*/
         //Expect the plans and checkbox exist
         await t
-            .expect(Selector('div').withText('Standard').exists).ok()
-            .expect(Selector('span').withText('CHOOSE STANDARD').exists).ok()
-            .expect(Selector('div').withText('Complete').exists).ok()
-            .expect(Selector('input').withAttribute('name', 'includeFloodProtection').exists).ok()
-            .expect(Selector('span').withText('CHOOSE COMPLETE').exists).ok()
+            .expect(div.withText('Standard').exists).ok()
+            .expect(span.withText('CHOOSE STANDARD').exists).ok()
+            .expect(div.withText('Complete').exists).ok()
+            .expect(input.withAttribute('name', 'includeFloodProtection').exists).ok()
+            .expect(span.withText('CHOOSE COMPLETE').exists).ok()
         //Expect the plans to have prices greater than $0 and the flood addition to be greater than 0 if it exists
-        var standard = parseInt(await getDigits(await Selector('h3').nth(1).innerText))
-        var complete = parseInt(await getDigits(await Selector('h3').nth(3).innerText))
+        var standard = parseInt(await getDigits(await h3.nth(1).innerText))
+        var complete = parseInt(await getDigits(await h3.nth(3).innerText))
         console.log("Standard Cost: $" + standard)
         console.log("Complete Cost: $" + complete)
         await t
