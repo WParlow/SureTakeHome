346e502fd590f16013e0a0b156c08f9622774e4f
WParlow <Parlow.whitney@gmail.com>
2021-05-27 14:53:58 -0500

    adding package.json

--- SureQAExam.js
+++ SureQAExam.js
@@ -1,5 +1,10 @@
-import { Selector, t } from 'testcafe';
-import { ClientFunction } from 'testcafe';
+import {
+    Selector,
+    t
+} from 'testcafe';
+import {
+    ClientFunction
+} from 'testcafe';
 const getWindowLocation = ClientFunction(() => window.location);
 
 const span = Selector('span')
@@ -18,90 +23,88 @@ fixture `Sure QA Take Home Exam`
 
 test('Happy Path BDD Test', async t => {
 
-        /*Landing page*/
-        //Don't enter a zipcode
-        await t
-            .click(span.withText('GET A QUOTE'))
-            .expect(p.withText('Required').exists).ok()
-        await checkURL(false, 'building-material')
+    /*Landing page*/
+    //Don't enter a zipcode
+    await t
+        .click(span.withText('GET A QUOTE'))
+        .expect(p.withText('Required').exists).ok()
+    await checkURL(false, 'building-material')
 
-        //Enter an invalid zip code
-        await t
-            .typeText(input.withAttribute('name', 'postalCode'), '1234')
-            .click(span.withText('GET A QUOTE'))
-            .expect(p.withText('Invalid zip code').exists).ok()
-        await checkURL(false, 'building-material')
-        
-        //Enter a valid zip code
-        await t
-            .selectText(input.withAttribute('name', 'postalCode'))
-            .typeText(input.withAttribute('name', 'postalCode'), '12345')
-            .click(span.withText('GET A QUOTE'))
-        await checkURL(true, 'building-material')
+    //Enter an invalid zip code
+    await t
+        .typeText(input.withAttribute('name', 'postalCode'), '1234')
+        .click(span.withText('GET A QUOTE'))
+        .expect(p.withText('Invalid zip code').exists).ok()
+    await checkURL(false, 'building-material')
 
-        /*Building Materials:*/
-        //Select nothing
-        await t
-            .click(span.withText('NEXT'))
-        await checkURL(false, 'water-proximity')
-        //Select 'Bricks'
-        await t
-            .click(input.withAttribute('value', 'bricks'))
-            .click(span.withText('NEXT'))
-        await checkURL(true, 'water-proximity')
+    //Enter a valid zip code
+    await t
+        .selectText(input.withAttribute('name', 'postalCode'))
+        .typeText(input.withAttribute('name', 'postalCode'), '12345')
+        .click(span.withText('GET A QUOTE'))
+    await checkURL(true, 'building-material')
 
-        /*Water Proximity:*/
-        //Select nothing
-        await t
-            .click(span.withText('NEXT'))
-        await checkURL(false, 'quote')
-        //Select 'Yes'
-        await t
-            .click(input.withAttribute('name', 'waterProximity').withAttribute('value', 'true'))
-            .click(span.withText('NEXT'))
-        await checkURL(true, 'quote')
+    /*Building Materials:*/
+    //Select nothing
+    await t
+        .click(span.withText('NEXT'))
+    await checkURL(false, 'water-proximity')
+    //Select 'Bricks'
+    await t
+        .click(input.withAttribute('value', 'bricks'))
+        .click(span.withText('NEXT'))
+    await checkURL(true, 'water-proximity')
 
-        /*Quote*/
-        //Expect the plans and checkbox exist
-        await t
-            .expect(div.withText('Standard').exists).ok()
-            .expect(span.withText('CHOOSE STANDARD').exists).ok()
-            .expect(div.withText('Complete').exists).ok()
-            .expect(input.withAttribute('name', 'includeFloodProtection').exists).ok()
-            .expect(span.withText('CHOOSE COMPLETE').exists).ok()
-        //Expect the plans to have prices greater than $0 and the flood addition to be greater than 0 if it exists
-        var standard = parseInt(await getDigits(await h3.nth(1).innerText))
-        var complete = parseInt(await getDigits(await h3.nth(3).innerText))
-        console.log("Standard Cost: $" + standard)
-        console.log("Complete Cost: $" + complete)
-        await t
-            .expect(standard).gt(0)
-            .expect(complete).gt(0)
-            .expect(complete).gt(standard)
-        try{
-            var flood = parseInt(await getDigits(await Selector('.MuiTypography-body1').innerText))
-            console.log("Flood Cost: $" + flood)
-            await t.expect(flood).gt(0)
-        }
-        catch (e){
-            console.log('Flood protection is included in the price.')
-        }
-        
-        await t.wait(5000)
+    /*Water Proximity:*/
+    //Select nothing
+    await t
+        .click(span.withText('NEXT'))
+    await checkURL(false, 'quote')
+    //Select 'Yes'
+    await t
+        .click(input.withAttribute('name', 'waterProximity').withAttribute('value', 'true'))
+        .click(span.withText('NEXT'))
+    await checkURL(true, 'quote')
+
+    /*Quote*/
+    //Expect the plans and checkbox exist
+    await t
+        .expect(div.withText('Standard').exists).ok()
+        .expect(span.withText('CHOOSE STANDARD').exists).ok()
+        .expect(div.withText('Complete').exists).ok()
+        .expect(input.withAttribute('name', 'includeFloodProtection').exists).ok()
+        .expect(span.withText('CHOOSE COMPLETE').exists).ok()
+    //Expect the plans to have prices greater than $0 and the flood addition to be greater than 0 if it exists
+    var standard = parseInt(await getDigits(await h3.nth(1).innerText))
+    var complete = parseInt(await getDigits(await h3.nth(3).innerText))
+    console.log("Standard Cost: $" + standard)
+    console.log("Complete Cost: $" + complete)
+    await t
+        .expect(standard).gt(0)
+        .expect(complete).gt(0)
+        .expect(complete).gt(standard)
+    try {
+        var flood = parseInt(await getDigits(await Selector('.MuiTypography-body1').innerText))
+        console.log("Flood Cost: $" + flood)
+        await t.expect(flood).gt(0)
+    } catch (e) {
+        console.log('Flood protection is included in the price.')
+    }
+
+    await t.wait(3000)
 });
 
-async function checkURL(bool, str){
+async function checkURL(bool, str) {
     var url = getWindowLocation();
 
-    if (bool == true){
+    if (bool == true) {
         await t.expect(url).contains(new RegExp(str))
-    }
-    else {
+    } else {
         await t.expect(url).notContains(new RegExp(str))
     }
 }
 
-async function getDigits(str){
+async function getDigits(str) {
     var digits = str.match(/\d+/g)
     digits = digits[0]
     return digits
